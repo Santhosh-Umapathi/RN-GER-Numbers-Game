e614955414659b821e7be2516019d89cc6f1de54
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-16 19:25:22 +0200

    Home screen with textinput and button created

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,11 +15,18 @@ export default class HomeScreen extends Component
 
 	render()
 	{
-		return (
-			<View>
-				<Text> HomeScreen </Text>
-			</View>
-			);
+	return (
+	<View style={styles.containerView}>
+		<Text style={styles.headerText}> Start the Game </Text>
+				
+		<TextInput style={styles.inputStyle} />
+	
+		<View style={styles.buttonView}>
+			<Button title="Reset" onPress={() => {}} style={styles.buttonStyle} />
+			<Button title="Continue" onPress={() => {}} style={styles.buttonStyle} />
+		</View>
+	</View>
+  );
 	}
 }
 
@@ -27,5 +34,34 @@ const styles = StyleSheet.create({
 	containerView:
 	{
 		flex:1
+	},
+	headerText:
+	{
+		fontSize: 30,
+		textAlign: 'center',
+		marginTop: 20
+	},
+	inputStyle:
+	{
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: 'pink',
+		paddingVertical: 15,
+		paddingHorizontal: 10,
+		margin: 20,
+		fontSize: 20
+		
+	},
+	buttonView:
+	{
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+	},
+	buttonStyle:
+	{
+		width: '100%',
+		maxWidth: '80%',
 	}
+
 });
