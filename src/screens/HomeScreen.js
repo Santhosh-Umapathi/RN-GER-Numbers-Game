ea0e3f6300331fd1f979ea919ab988bce59b01fa
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-17 12:05:55 +0200

    Button config complete with methods

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,56 +1,109 @@
-import React, { Component } from 'react';
-import { Text, TextInput, StyleSheet, View, TouchableOpacity, Button, Flatlist } from 'react-native';
+import React, { Component } from "react";
+import {
+ Text,
+ StyleSheet,
+ View,
+ TouchableOpacity,
+ Button,
+ Flatlist,
+ TouchableWithoutFeedback,
+ Keyboard,
+} from "react-native";
 //Colors
 import Colors from "../constants/Colors";
 //Card
-import Card from '../components/Card';
+import Card from "../components/Card";
 //TextInput
-import TextField from '../components/TextField';
+import TextField from "../components/TextField";
 
-export default class HomeScreen extends Component
-{
-	constructor(props)
-	{
-		super(props);
-		this.state =
-		{
-			text:''
-		};
-	}
+export default class HomeScreen extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   inputVal: "",
+   confirmed: false,
+   savedVal: null,
+  };
+ }
 
+ //Handle Input with only number, no special chars
+ inputHandler = (inputText) => {
+  this.setState({
+   inputVal: inputText.replace(/[^0-9]/g, ""),
+  });
+ };
 
-render()
-{
-return (
- <View style={styles.containerView}>
-  <Text style={styles.headerText}> Start the Game </Text>
+ //Reset Number
+ resetHandler = () => {
+  this.setState({ inputVal: "", confirmed: false });
+ };
 
-		<Card style={styles.cardView}>
-			<Text style={{ fontSize: 20, textAlign:'center', marginVertical: 10,}}>Enter a Number</Text>
-	<TextField
-		keyboardType="number-pad"
-		blurOnSubmit
-		maxLength={2}	
-		//style={styles.inputStyle}		
-	/>
-   <View style={styles.buttonView}>
-    <Button
-     title="Reset"
-     onPress={() => {}}
-     style={styles.buttonStyle}
-     color={Colors.secondaryColor}
-    />
-    <Button
-     title="Continue"
-     onPress={() => {}}
-     style={styles.buttonStyle}
-     color={Colors.primaryColor}
-    />
-   </View>
-  </Card>
- </View>
-);
-	}
+ //Confirm Number
+ confirmHandler = () => {
+  const chosenNumber = parseInt(this.state.inputVal);
+  if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+   return;
+  }
+  this.setState({
+   confirmed: true,
+   inputVal: "",
+   savedVal: chosenNumber,
+  });
+ };
+
+	confirmedNumber; //Empty Confirmed number declared
+
+ render() {
+  if (this.state.confirmed) {
+   this.confirmedNumber = <Text> Chosen Number is: {this.state.savedVal}</Text>;
+  }
+
+  return (
+   <TouchableWithoutFeedback
+    onPress={() => {
+     Keyboard.dismiss();
+    }}
+   >
+    <View style={styles.containerView}>
+     <Text style={styles.headerText}> Start the Game </Text>
+
+     <Card style={styles.cardView}>
+      <Text
+       style={{
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 10,
+       }}
+      >
+       Enter a Number
+      </Text>
+      <TextField
+       keyboardType="number-pad"
+       blurOnSubmit
+       maxLength={2}
+       onChangeText={(inputText) => this.inputHandler(inputText)}
+       value={this.state.inputVal}
+      />
+      <View style={styles.buttonView}>
+       <Button
+        title="Reset"
+        onPress={this.resetHandler}
+        style={styles.buttonStyle}
+        color={Colors.secondaryColor}
+       />
+       <Button
+        title="Continue"
+        onPress={this.confirmHandler}
+        style={styles.buttonStyle}
+        color={Colors.primaryColor}
+       />
+      </View>
+     </Card>
+     {this.confirmedNumber}
+    </View>
+   </TouchableWithoutFeedback>
+  );
+ }
 }
 
 const styles = StyleSheet.create({
@@ -63,12 +116,14 @@ const styles = StyleSheet.create({
   marginTop: 20,
  },
  inputStyle: {
-	 width: 40,
+  width: 40,
  },
  buttonView: {
   flexDirection: "row",
   justifyContent: "space-around",
   alignItems: "center",
+  marginTop: 20,
+  marginBottom: 10,
  },
  buttonStyle: {
   width: "100%",
