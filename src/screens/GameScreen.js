ee5a5b870bf4526c3183b0b95cf7f17d7f77c386
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-18 10:00:56 +0200

    Game screen converted to functional component

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,100 +1,86 @@
-import React, { Component, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
  View,
  Text,
- TextInput,
  StyleSheet,
- TouchableOpacity,
  Button,
  Alert,
 } from "react-native";
+//Components
 import Card from "../components/Card";
 import Colors from "../constants/Colors";
 
 //Generating Random number for guess
-const generateRN = (min, max, exclude) => {
- min = Math.ceil(min);
- max = Math.floor(max);
- const randomNum = Math.floor(Math.random() * (max - min)) + min;
- if (randomNum === exclude) {
-  return generateRN(min, max, exclude);
- } else {
-  return randomNum;
- }
+const generateRN = (min, max, exclude) =>
+{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNum = Math.floor(Math.random() * (max - min)) + min;
+  if (randomNum === exclude)
+  {
+    return generateRN(min, max, exclude);
+  }
+  else
+  {
+    return randomNum;
+  }
 };
 
-export default class GameScreen extends Component {
- constructor(props) {
-  super(props);
-	 this.state = {
-		 currentLow: 1,
-		 currentHigh: 100,
-   userChoice: this.props.navigation.getParam("userChoice"),
-   currentRN: generateRN(1, 100, props.userNumber),
-  };
- }
+const GameScreen = ( props) =>
+{
+
+
+  const userChoice = props.navigation.getParam('userChoice');
+
+  
+  const [currentRN, setCurrentRN] = useState(generateRN(1, 100, userChoice));
+
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
 
  
 
  nextNumberHandler = (valueType) => {
-  if (
-   (valueType === "lower" && this.state.currentRN < this.state.userChoice) ||
-   (valueType === "greater" && this.state.currentRN > this.state.userChoice)
-  ) {
-   Alert.alert("Don 't lie", "No Cheating", [
-    { text: "Sorry", style: "cancel" },
-   ]);
-   return;
+  if(
+      (valueType === "lower" && currentRN < userChoice) ||
+      (valueType === "greater" && currentRN > userChoice)
+  )
+  {
+    Alert.alert("Don 't lie", "No Cheating", [{ text: "Sorry", style: "cancel" }]);
+    return;
   }
 	 
 	if (valueType === "lower")
-	{
-    this.setState({ currentHigh: this.state.currentRN })
-      const nextNum = generateRN(
-       this.state.currentLow,
-       this.state.currentHigh,
-       this.state.currentRN
-      );
-      this.setState({ currentRN: nextNum });
+  {
+    currentHigh.current = currentRN;
 	}
-	else if (valueType === "greater") {
-    this.setState({ currentLow: this.state.currentRN })
-      const nextNum = generateRN(
-       this.state.currentLow,
-       this.state.currentHigh,
-       this.state.currentRN
-      );
-      this.setState({ currentRN: nextNum });
-    }
-	 const nextNum = generateRN(
-   this.state.currentLow,
-   this.state.currentHigh,
-   this.state.currentRN
-  );
-  this.setState({ currentRN: nextNum });
-	 
+  else if (valueType === "greater")
+  {
+      currentLow.current = currentRN;
+  }
+	const nextNum = generateRN(currentLow.current,currentHigh.current,currentRN);
+  setCurrentRN(nextNum);
  };
 
- render() {
+ 
   return (
    <View style={styles.containerView}>
     <Text style={{ fontSize: 25, alignSelf: "center", marginTop: 10 }}>
      Opponent's Guess
     </Text>
 
-    <Text style={styles.textStyle}>{this.state.currentRN}</Text>
+    <Text style={styles.textStyle}>{currentRN}</Text>
 
     <Card style={{ marginHorizontal: 60 }}>
      <View style={styles.buttonView}>
       <Button
        title="Lower"
-       onPress={this.nextNumberHandler.bind(this, 'lower')}
+       onPress={()=> nextNumberHandler('lower')}
        style={styles.buttonStyle}
       />
       <Button
        title="Greater"
-       onPress={
-        this.nextNumberHandler.bind(this, 'greater')}
+       onPress={ ()=> nextNumberHandler('greater')}
        style={styles.buttonStyle}
       />
      </View>
@@ -102,7 +88,7 @@ export default class GameScreen extends Component {
    </View>
   );
  }
-}
+
 
 const styles = StyleSheet.create({
  containerView: {
@@ -130,8 +116,11 @@ const styles = StyleSheet.create({
  },
 });
 
-GameScreen.navigationOptions = ({ navigation }) => {
- return {
-  headerTitle: "Game Screen",
- };
+GameScreen.navigationOptions = ({ navigation }) =>
+{
+  return {
+    headerTitle: "Game Screen",
+  };
 };
+
+export default GameScreen;
